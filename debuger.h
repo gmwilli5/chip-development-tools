45cd3a4f3c96dde4c002de75da147456c3906343
gmwilli5 <gmwilli5@ncsu.edu>
2014-08-09 12:07:30 -0400

    fixed problem and added debug classes

--- debuger.h
+++ debuger.h
@@ -1,13 +1,12 @@
 #ifndef DEBUGER_H_INCLUDED
 #define DEBUGER_H_INCLUDED
-#include "chip8_with_debug.cpp"
-#include <fstream>
-/*class debuger{
+#include "chip8_with_debug.h"
+class debuger{
     public:
         chip8_debug* chip;
     public:
         debuger();
         void print_info();
         void save_info();
-};*/
+};
 #endif // DEBUGER_H_INCLUDED
